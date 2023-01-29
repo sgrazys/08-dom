204e46ef81f7406cc015f7c6b63df21d57c70f63
sgrazys <s.grazys@gmail.com>
2023-01-29 17:31:52 +0200

    DOM

--- js/main.js
+++ js/main.js
@@ -1,9 +1,39 @@
 const h1DOM = document.getElementsByTagName('h1')[0];
-console.log(h1DOM);
 
 const text = h1DOM.textContent;
-console.log(`"${text}"`);
 
 const newText = text.trim().replace('rytas', 'diena');
 
 h1DOM.innerText = newText;
+
+const colors = ['green', 'red', 'blue', 'orange'];
+
+for (const color of colors) {
+    h1DOM.classList.add(color)
+}
+
+h1DOM.classList.remove('red');
+
+h1DOM.classList.toggle('blue');
+h1DOM.classList.toggle('orange');
+h1DOM.classList.toggle('pink');
+
+const arTuri1 = h1DOM.classList.contains('agurkas');
+console.log(arTuri1);
+
+const arTuri2 = h1DOM.classList.contains('pink');
+console.log(arTuri2);
+
+console.log([h1DOM]);
+
+const [minusDOM, plusDOM] = document.querySelectorAll('button');
+const scoreDOM = document.querySelector('.score');
+
+let score = 0;
+minusDOM.addEventListener('click', () => {
+    scoreDOM.innerText = --score;
+});
+
+plusDOM.addEventListener('click', () => {
+    scoreDOM.innerText = ++score;
+});
